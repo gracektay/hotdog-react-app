940bb03132e9b8b6e9ab292a316510d1b200bbe9
Grace Tay <gktaydesigns@gmail.com>
2018-01-12 23:08:02 -0500

    Designed view for footer component

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Thought Process:
   > I noticed that there's a lot of repeated elements on this page, so a component architecure with React made sense for this project. Plus it seemed like a fun challenge!
   - Step 1 - Compile all the necessary assets into appropriate folders, such as photos and fonts.
   - Step 2 - Identify the major components and subcomponents and create appropriate directories and files accordingly.
+  - Step 3 - Design Footer *No functionality added yet*
---
+++ src/App/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css'
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="copyright">
+                    ® 2016 Dirty Dogs all rights reserved
+                </div>
+                <div className="contact-links">
+                    274 Marconi Blvd. Columbus, Ohio 43215    |   614.538.0095   |   Contact Us
+                </div>
+            </div>
+        );
+    }
+}
+
