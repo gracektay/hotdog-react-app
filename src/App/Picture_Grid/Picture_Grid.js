a1c48c194b915af45cec2b3025fca3320d5aad24
Grace Tay <gktaydesigns@gmail.com>
2018-01-14 15:33:20 -0500

    Refactoring. Changed the way pictures are handled, now uses percent width of screen and uses image component. Added media queries to adjust height so it responds to screen width.

--- src/App/Picture_Grid/Picture_Grid.js
+++ src/App/Picture_Grid/Picture_Grid.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Picture_Grid.css";
 import photo1 from "../Assets/picture_grid/photo_1.png";
 import photo2 from "../Assets/picture_grid/photo_2.png";
@@ -14,32 +14,37 @@ import photo11 from "../Assets/picture_grid/photo_11.png";
 import photo12 from "../Assets/picture_grid/photo_12.png";
 import instagram from "../Assets/icons/instagram.png";
 
-export default class Picture_Grid extends React.Component {
-  render() {
+export default class Picture_Grid extends Component {
+    render() {
+        return (
+            <div className="picture-grid">
+                <div className="row">
+                    <Image imageRef={photo1} alt="hot dog 1" />
+                    <Image imageRef={photo2} alt="hot dog 2" />
+                    <Image imageRef={photo3} alt="hot dog 3" />
+                    <Image imageRef={photo4} alt="hot dog 4" />
+                    <Image imageRef={photo5} alt="hot dog 5" />
+                    <Image optSize="img-sm" imageRef={photo6} alt="hot dog 6" />
+                </div>
+                <div className="row">
+                    <Image imageRef={photo7} alt="hot dog 7" />
+                    <Image imageRef={photo8} alt="hot dog 8" />
+                    <Image imageRef={photo9} alt="hot dog 9" />
+                    <Image imageRef={photo10} alt="hot dog 10" />
+                    <Image imageRef={photo11} alt="hot dog 11" />
+                    <Image optSize="img-sm" imageRef={photo12} alt="hot dog 12" />
+                </div>
+                <div className="social">
+                    <img src={instagram} alt="Instagram" />
+                    #hotdogs
+                </div>
+            </div>
+        );
+    }
+}
+
+function Image(props) {
     return (
-        <div className="picture-grid">
-      <ul>
-        <div className="row">
-          <li><img src={photo1} alt="hot dog 1" /></li>
-          <li><img src={photo2} alt="hot dog 2" /></li>
-          <li><img src={photo3} alt="hot dog 3" /></li>
-          <li><img src={photo4} alt="hot dog 4" /></li>
-          <li><img src={photo5} alt="hot dog 5" /></li>
-          <li><img src={photo6} alt="hot dog 6" /></li>
-        </div>
-        <div className="row">
-          <li><img src={photo7} alt="hot dog 7" /></li>
-          <li><img src={photo8} alt="hot dog 8" /></li>
-          <li><img src={photo9} alt="hot dog 9" /></li>
-          <li><img src={photo10} alt="hot dog 10" /></li>
-          <li><img src={photo11} alt="hot dog 11" /></li>
-          <li><img src={photo12} alt="hot dog 12" /></li>
-        </div>
-      </ul>
-      <div className="social">
-            <img src={instagram} alt="Instagram" />#hotdogs
-        </div>
-      </div>
+        <img className={props.optSize} src={props.imageRef} alt={props.alt} />
     );
-  }
 }
