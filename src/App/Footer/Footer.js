374702dff7f637290455b029162110b17cbf8d5f
Grace Tay <gktaydesigns@gmail.com>
2018-01-13 23:08:40 -0500

    Changed footer text to be links on the right hand side

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -2,14 +2,30 @@ import React from "react";
 import "./Footer.css";
 
 export default class Footer extends React.Component {
-  render() {
+    render() {
+        return (
+            <div className="footer">
+                <div className="copyright">
+                    ®2016 Dirty Dogs all rights reserved
+                </div>
+                <ul className="contact-links">
+                    <FooterLink
+                        url="https://goo.gl/maps/5hL27CGCpbr"
+                        target="_blank"
+                        title="274 Marconi Blvd. Columbus, Ohio 43215"
+                    />
+                    <FooterLink url="#" title="614.538.0095" />
+                    <FooterLink url="#" title="Contact Us" />
+                </ul>
+            </div>
+        );
+    }
+}
+
+function FooterLink(props) {
     return (
-      <div className="footer">
-        <div className="copyright">®2016 Dirty Dogs all rights reserved</div>
-        <div className="contact-links">
-          274 Marconi Blvd.Columbus, Ohio 43215 | 614.538 .0095 | Contact Us
-        </div>
-      </div>
+        <li className="FooterLink">
+            <a target={props.target} href={props.url}>{props.title}</a>
+        </li>
     );
-  }
 }
